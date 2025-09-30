b324f97f2561622c562ae14a5f0ced65b51057af
kolgotik <radikppg@gmail.com>
2025-09-30 13:29:27 +0500

    improved card encryption

--- src/main/java/com/example/bankcards/util/SimpleCardEncryptConverter.java
+++ src/main/java/com/example/bankcards/util/SimpleCardEncryptConverter.java
@@ -3,20 +3,38 @@ package com.example.bankcards.util;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
 @Converter
 public class SimpleCardEncryptConverter implements AttributeConverter<String, String> {
 
+    //for test only
+    private static final String SECRET = "1234567890ABCDEF";
+    private static final SecretKeySpec KEY = new SecretKeySpec(SECRET.getBytes(), "AES");
+
     @Override
     public String convertToDatabaseColumn(String attribute) {
         if (attribute == null) return null;
-        return Base64.getEncoder().encodeToString(attribute.getBytes());
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, KEY);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(attribute.getBytes()));
+        } catch (Exception e) {
+            throw new RuntimeException("Encryption error", e);
+        }
     }
 
     @Override
     public String convertToEntityAttribute(String dbData) {
         if (dbData == null) return null;
-        return new String(Base64.getDecoder().decode(dbData));
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, KEY);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(dbData)));
+        } catch (Exception e) {
+            throw new RuntimeException("Decryption error", e);
+        }
     }
 }
